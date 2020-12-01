250df6c99105d4cc52f7f34c016aeb9c3c275288
LayneHansen <layne.d.hansen@gmail.com>
2020-11-30 21:06:51 -0700

    created topcard component, added styling

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import TopCard from './TopCard';
+
+function App() {
+  return (
+    <TopCard />
+
+  );
+}
+
+export default App;
---
+++ src/TopCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './TopCard.css';
+
+function TopCard () {
+    return (
+        <>
+        <div class="card text-center">
+            <div class="card-header bg-dark">
+                Business Name
+            </div>
+            <div class="card-body" id="card-body">
+                <h3 class="card-title">EMPLOYEE SEARCH</h3>
+            </div>
+            <nav class="navbar navbar-dark bg-dark">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" 
+                    type="search" 
+                    placeholder="Search" 
+                    aria-label="Search" />
+                    <button class="btn btn-outline-success my-2 my-sm-0" 
+                    type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+        </>
+    )
+
+}
+
+
+
+export default TopCard;
