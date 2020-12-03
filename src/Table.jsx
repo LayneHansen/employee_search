dfc39c1ea75580afbb6ce51a5bda51ed75cd0d22
LayneHansen <layne.d.hansen@gmail.com>
2020-12-03 09:46:26 -0700

    updated fetch call

--- src/Table.jsx
+++ src/Table.jsx
@@ -15,7 +15,7 @@ class Table extends React.Component {
   fetchEmployees = async () => {
     try {
       const response = await fetch(
-        "https://randomuser.me/?inc=id,first,last,email,phone,city,state"
+        "https://randomuser.me/?inc=value,first,last,email,phone,city,state"
       );
       const data = await response.json();
       this.setState({ employees: data });
@@ -39,7 +39,6 @@ class Table extends React.Component {
                     <th>State</th>
                 </tr>
             </thead>
-        </ReactBootstrap.Table>
             <tbody>
                 { this.state.employees.map( (id) => (
                     <tr> </tr>
@@ -48,52 +47,11 @@ class Table extends React.Component {
                 )
   }
             </tbody>
+        </ReactBootstrap.Table>
         </>
     )
 }
 
-// const employees = [
-//     {id: "", first: "", last: "", email: "", phone: "", city: "", state: ""},
-// ]
-
-// const renderEmployee = (employee, id) => {
-//     return(
-//         <tr key={id}>
-//             <td>{employee.first}</td>
-//             <td>{employee.last}</td>
-//             <td>{employee.email}</td>
-//             <td>{employee.phone}</td>
-//             <td>{employee.city}</td>
-//             <td>{employee.state}</td>
-//         </tr>
-//     )
-// }
-
-// $.ajax({
-//     url: 'https://randomuser.me/?inc=id,name,email,phone,city,state',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
-
-// return (
-    // <>
-    // <ReactBootstrap.Table striped bordered hover>
-    //     <thead>
-    //         <th>Emp #</th>
-    //         <th>First Name</th>
-    //         <th>Last Name</th>
-    //         <th>Email</th>
-    //         <th>Phone</th>
-    //         <th>City</th>
-    //         <th>State</th>
-    //     </thead>
-    // </ReactBootstrap.Table>
-    //     <tbody>
-    //         {employees.map(renderEmployee)}
-    //     </tbody>
-    // </>
 }
 
 export default Table;
